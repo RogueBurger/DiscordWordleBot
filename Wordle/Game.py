4a090b39fda857135046e89e1fd825bd7473db7a
RogueBurger <anthonyjallan@gmail.com>
2021-12-18 17:53:59 -0600

    More cleanup

--- Wordle/Game.py
+++ Wordle/Game.py
@@ -1,20 +1,23 @@
+from typing import Optional
+
 from Helpers.RandomText import RandomText
 from Wordle.Words import Words
 from Wordle.Canvas import Canvas, Image, Glyph, GlyphColor
 
-class Game:
-    CORRECT = 'correct'
-    INCORRECT = 'incorrect'
-    INVALID = 'invalid'
 
-    def __init__(self, canvas: Canvas, word_length=5):
+class Game:
+    CORRECT: str = 'correct'
+    INCORRECT: str = 'incorrect'
+    INVALID: str = 'invalid'
+
+    def __init__(self, canvas: Canvas, word_length: int = 5):
         self.word_length: int = word_length
         self.num_guesses: int = 0
         self.target: str = ''
         self.definition: str = ''
         self.guesses: list = []
-        self.progress: Image = None
-        self.canvas = canvas
+        self.progress: Optional[Image] = None
+        self.canvas: Canvas = canvas
 
         self.generate_target()
 
@@ -29,7 +32,9 @@ class Game:
             return self.INVALID, f'Your guesses must be {self.word_length} letters long.', None
 
         if lowered_word == self.target:
-            return self.CORRECT, f'{word} is the correct answer! Congrats! {word}: {self.definition}', self.draw_word(word)
+            return self.CORRECT, \
+                   f'{word} is the correct answer! Congrats! {word}: {self.definition}', \
+                   self.draw_word(word)
 
         if not Words.get_word(lowered_word):
             return self.INVALID, f'{word} is not a word, you {RandomText.idiot()}', None
@@ -44,18 +49,6 @@ class Game:
     def get_history(self) -> list:
         return self.guesses
 
-    def __format_word(self, word: str) -> str:
-        return ' '.join([self.__format_letter(letter.lower(), index) for index, letter in enumerate(word)])
-
-    def __format_letter(self, letter: str, index: int) -> str:
-        if self.target[index] == letter:
-            return chr(ord(letter) + 127215)
-
-        if letter in self.target:
-            return chr(ord(letter) + 9301)
-
-        return letter.capitalize()
-
     def draw_word(self, word):
         return self.canvas.draw_word([self.draw_letter(letter.lower(), index) for index, letter in enumerate(word)])
 
@@ -65,6 +58,5 @@ class Game:
 
         if letter in self.target:
             return self.canvas.draw_char(letter.upper(), GlyphColor.WARM)
-
 
         return self.canvas.draw_char(letter.upper(), GlyphColor.COLD)
