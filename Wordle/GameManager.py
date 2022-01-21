c72786ec43b7493b6f10b84935ba11ca68370e49
hobochili <hobochili@pm.me>
2022-01-20 23:59:22 -0600

    Fix handling for DM games

--- Wordle/GameManager.py
+++ Wordle/GameManager.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from discord import TextChannel
+from discord import TextChannel, DMChannel
 
 from Wordle.Canvas import Canvas
 from Wordle.Game import Game
@@ -13,30 +13,34 @@ class GameManager:
         self.store: Store = backend
         self.canvas: Canvas = Canvas()
 
+    @staticmethod
+    def server_id(channel: TextChannel) -> int:
+        return channel.guild.id if not isinstance(channel, DMChannel) else 0
+
     def create_game(self, word_length: int, mode: str) -> Game:
         return Game(word_length=word_length, mode=mode, canvas=self.canvas)
 
     @asynccontextmanager
     async def lock(self, channel: TextChannel) -> Lock:
         """ raises LockNotFoundError """
-        async with self.store.lock(channel.guild.id, channel.id):
+        async with self.store.lock(GameManager.server_id(channel), channel.id):
             yield
 
     async def add_game(self, channel: TextChannel, game: Game) -> Game:
         """ raises GameNotAddedError """
-        return await self.store.add_game(channel.guild.id, channel.id, game)
+        return await self.store.add_game(GameManager.server_id(channel), channel.id, game)
 
     async def get_current_game(self, channel: TextChannel) -> Game:
         """ raises GameNotFoundError """
-        game = await self.store.get_game(channel.guild.id, channel.id)
+        game = await self.store.get_game(GameManager.server_id(channel), channel.id)
         if not hasattr(game, 'canvas') or game.canvas is None:
             game.canvas = self.canvas
 
         return game
 
     async def stop_current_game(self, channel: TextChannel) -> bool:
-        return await self.store.remove_game(channel.guild.id, channel.id)
+        return await self.store.remove_game(GameManager.server_id(channel), channel.id)
 
     async def update_game(self, channel: TextChannel, game: Game) -> Game:
         """ raises GameNotUpdatedError """
-        return await self.store.update_game(channel.guild.id, channel.id, game)
+        return await self.store.update_game(GameManager.server_id(channel), channel.id, game)
